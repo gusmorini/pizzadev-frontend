979114ec6d38eda0fe4a420fe314f1d04fb63a08
Gustavo Morini <gustavommh@gmail.com>
2023-01-19 17:44:15 -0300

    page dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Head from "@/components/Head";
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head title="dashboard" />
+      <div>
+        <h1>dashboard</h1>
+      </div>
+    </>
+  );
+}
