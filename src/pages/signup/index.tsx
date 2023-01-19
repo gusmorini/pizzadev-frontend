ee8d48b2b1570263e1a170ce922aa8201cc50bb5
Gustavo Morini <gustavommh@gmail.com>
2023-01-18 23:34:57 -0300

    signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,29 +1,29 @@
 import Image from "next/image";
 import Head from "@/components/Head";
 import styles from "@/styles/home.module.scss";
-import logoImg from "../../public/logo.svg";
+import logoImg from "@/../public/logo.svg";
 
 import { Input, TextArea } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
-      <Head title="faça seu login" />
+      <Head title="faça seu cadastro" />
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="logo pizza dev" />
         <div className={styles.login}>
+          <h1>Faça seu cadastro</h1>
           <form action="#">
-            <Input type="email" placeholder="seu e-mail" />
+            <Input type="text" placeholder="seu nome" />
+            <Input type="email" placeholder={"seu e-mail"} />
             <Input type="password" placeholder="sua senha" />
-            <Button loading={false}>Acessar</Button>
+            <Button loading={false}>Cadastrar</Button>
           </form>
-          <Link href="/signup">
-            <span className={styles.link}>
-              Não possui uma conta? Cadastre-se
-            </span>
+          <Link href="/">
+            <span className={styles.link}>Já possui conta? Faça o login</span>
           </Link>
         </div>
       </div>
