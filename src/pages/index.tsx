dd580459f3abc73308ace1ac4e6106872a296c8d
Gustavo Morini <gustavommh@gmail.com>
2023-01-19 09:21:56 -0300

    auth context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useContext } from "react";
 import Image from "next/image";
 import Head from "@/components/Head";
 import styles from "@/styles/home.module.scss";
@@ -8,17 +9,33 @@ import { Button } from "@/components/ui/Button";
 
 import Link from "next/link";
 
+import { AuthContext } from "@/contexts/AuthContext";
+
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    console.log(event);
+    const data = {
+      email: "teste@test",
+      password: "123456",
+    };
+    await signIn(data);
+  }
+
   return (
     <>
       <Head title="faça seu login" />
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="logo pizza dev" />
         <div className={styles.login}>
-          <form action="#">
-            <Input type="email" placeholder="seu e-mail" />
-            <Input type="password" placeholder="sua senha" />
-            <Button loading={false}>Acessar</Button>
+          <form onSubmit={handleLogin}>
+            <Input name="email" type="email" placeholder="seu e-mail" />
+            <Input name="password" type="password" placeholder="sua senha" />
+            <Button loading={false} type="submit">
+              Acessar
+            </Button>
           </form>
           <Link href="/signup">
             <span className={styles.link}>
