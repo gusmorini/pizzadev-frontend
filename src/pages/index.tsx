e3890e178cb860878a57aed76e12d5d39c9650bb
Gustavo Morini <gustavommh@gmail.com>
2023-01-18 22:49:54 -0300

    home login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Head from "@/components/Head";
+import styles from "@/styles/home.module.scss";
+import logoImg from "../../public/logo.svg";
+
+import { Input, TextArea } from "@/components/ui/Input";
+import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
     <>
       <Head title="faça seu login" />
-      <div>
-        <h1>HOME</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="logo pizza dev" />
+        <div className={styles.login}>
+          <form action="#">
+            <Input type={"email"} placeholder={"seu e-mail"} />
+            <Input type={"password"} placeholder="sua senha" />
+            <Button loading={false}>Acessar</Button>
+          </form>
+        </div>
       </div>
     </>
   );
