4909895a01a5646beab6738d520842cd180d619e
Gustavo Morini <gustavommh@gmail.com>
2023-01-24 21:14:23 -0300

    component input select

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,8 +1,17 @@
-import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import {
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+  TextareaHTMLAttributes,
+  ReactNode,
+} from "react";
+
 import styles from "./styles.module.scss";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  children: ReactNode;
+}
 
 export function Input({ ...rest }: InputProps) {
   return <input className={styles.input} {...rest} />;
@@ -10,4 +19,12 @@ export function Input({ ...rest }: InputProps) {
 
 export function TextArea({ ...rest }: TextareaProps) {
   return <textarea className={styles.input} {...rest}></textarea>;
+}
+
+export function Select({ children, ...rest }: SelectProps) {
+  return (
+    <select className={styles.input} {...rest}>
+      {children}
+    </select>
+  );
 }
